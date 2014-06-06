46acc4be1530c045a023c6f38687a551fa0392fb
Hao Wei <imwillmouse@gmail.com>
2014-06-06 13:30:35 +0800

    :fries::fries::fries::fries:

--- lib/local_mail/engine.rb
+++ lib/local_mail/engine.rb
@@ -7,5 +7,9 @@ module LocalMail
     initializer "local_mail.add_delivery_method" do
       ActionMailer::Base.add_delivery_method :local_mail, LocalMail::DeliveryMethod, location: Rails.root.join("tmp", "local_mail")
     end
+
+    initializer "local_mail.mkdir" do
+      Dir.mkdir('tmp/local_mail') if !Dir.exist?('tmp/local_mail')
+    end
   end
 end
