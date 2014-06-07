ae8fa532d800a6a63cde2c5c59062a79b3c7bd35
Hao Wei <imwillmouse@gmail.com>
2014-06-07 17:39:46 +0800

    :aerial_tramway::aerial_tramway::aerial_tramway:

--- app/controllers/local_mail/mails_controller.rb
+++ app/controllers/local_mail/mails_controller.rb
@@ -1,62 +1,66 @@
-require_dependency "local_mail/application_controller"
+class LocalMail::MailsController < ApplicationController
+  layout false
 
-module LocalMail
-  class MailsController < ApplicationController
-    layout false
+  before_filter :check_permission
 
-    def index
-      @emails = Array.new
+  def index
+    @emails = Array.new
 
-      Dir.chdir(Rails.root.join('tmp', 'local_mail')) do
-        Dir.glob("*") do |dir|
-          time = dir.split('_').first
-          subject = dir.split('_').second.gsub!('-',' ').humanize + " (To: <#{dir.split('_').third}>)"
-          href = dir.split('_').fourth + ( rich_style?(dir) ? '/rich': '/plain' )
-          @emails << { time: time, subject: subject, href: href }
-        end
+    in_mail_directory do |dir|
+	time = dir.split('_').first
+	subject = dir.split('_').second.gsub!('-',' ').humanize + " (To: <#{dir.split('_').third}>)"
+	href = dir.split('_').fourth + ( rich_style?(dir) ? '/rich': '/plain' )
+	@emails << { time: time, subject: subject, href: href }
+    end
+
+    @emails = @emails.sort_by { |email| email[:time] }.reverse.take(200)
+  end
+
+  def show
+    email_content = ''
+
+    in_mail_directory("*#{params[:hash]}") do |dir|
+      if params[:style] != 'plain' && rich_style?(dir)
+	email_content = File.read("#{dir}/rich.html")
+      else
+	email_content = File.read("#{dir}/plain.html")
       end
-
-      @emails = @emails.sort_by { |email| email[:time] }.reverse.take(200)
     end
 
-    def show
-      email_content = ''
+    fix_links(email_content)
 
-      Dir.chdir(Rails.root.join('tmp', 'local_mail')) do
-        Dir.glob("*#{params[:hash]}") do |dir|
-          if params[:style] != 'plain' && rich_style?(dir)
-            email_content = File.read("#{dir}/rich.html")
-          else
-            email_content = File.read("#{dir}/plain.html")
-          end
-        end
+    render text: email_content
+  end
+
+  def attachments
+    attachment = ''
+
+    in_mail_directory("*#{params[:hash]}") do |dir|
+      attachment = File.read("#{dir}/attachments/#{params[:name]}.#{params[:format]}")
+    end
+
+    render text: attachment
+  end
+
+  protected
+  def check_permission
+    redirect_to '/' unless current_user.role == 'manager'
+  end
+
+  private
+  def fix_links(text)
+    text.sub!(/(<a\s+(?:[^>]*?\s+)?href=)"([^"]*)"/, '\1' + '\2')
+  end
+
+  def rich_style?(dir)
+    File.exist?("#{dir}/rich.html")
+  end
+
+  def in_mail_directory(pattern = '*')
+    Dir.chdir(Rails.root.join('tmp', 'local_mail')) do
+      Dir.glob(pattern) do |dir|
+	yield dir
       end
-
-      fix_links(email_content)
-
-      render text: email_content
-    end
-
-    def attachments
-      attachment = ''
-
-      Dir.chdir(Rails.root.join('tmp', 'local_mail')) do
-        Dir.glob("*#{params[:hash]}") do |dir|
-          attachment = File.read("#{dir}/attachments/#{params[:name]}.#{params[:format]}")
-        end
-      end
-
-      render text: attachment
-    end
-
-    private
-
-    def fix_links(text)
-      text.sub!(/(<a\s+(?:[^>]*?\s+)?href=)"([^"]*)"/, '\1' + '\2')
-    end
-
-    def rich_style?(dir)
-      File.exist?("#{dir}/rich.html")
     end
   end
 end
